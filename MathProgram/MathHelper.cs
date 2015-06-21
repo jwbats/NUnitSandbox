b103cd5c80c0b4784551f064ec31ec791f3bf07c
jwbats <jwbats@gmail.com>
2015-06-21 16:07:07 +0200

    Initial commit.

---
+++ MathProgram/MathHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathProgram
+{
+	public class MathHelper
+	{
+
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		public int Subtract(int a, int b)
+		{
+			return a - b;
+		}
+
+	}
+}
