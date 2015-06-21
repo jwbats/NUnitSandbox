be06920246734a31a0b029931c7c50a4b1c36d8a
jwbats <jwbats@gmail.com>
2015-06-21 16:09:14 +0200

    Including file.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MathProgram;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class UnitTest1
+	{
+
+		[TestMethod]
+		public void Test_Add()
+		{
+			MathHelper mathHelper = new MathHelper();
+			int result = mathHelper.Add(10, 10);
+			Assert.AreEqual(20, result);
+		}
+
+
+
+
+		[TestMethod]
+		public void Test_Subtract()
+		{
+			MathHelper mathHelper = new MathHelper();
+			int result = mathHelper.Subtract(10, 10);
+			Assert.AreEqual(0, result);
+		}
+
+	}
+}
